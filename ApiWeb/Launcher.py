55f751218e2cf192660a19a1fcce6f471e652db2
Moad Razzaki <m.razzaki@biodiv-wind.com>
2024-01-30 15:13:24 +0100

    commit

--- ApiWeb/Launcher.py
+++ ApiWeb/Launcher.py
@@ -1,13 +1,16 @@
-from flask import Flask, request, jsonify
+import json
+from flask import Flask, jsonify
+from data_manager import DBConnector
 
 app = Flask(__name__)
+db_connector = DBConnector()
 
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    db = db_connector.connect()
+    data = db.packet_DHCP.find()  
+    result = [{"Type": item["Type"], "Ether": item["Ether"], "IP": item["IP"], "DHCP": item["DHCP"], "UDP": item["UDP"], "BOOTP": item["BOOTP"]} for item in data]
+    return jsonify(result)
 
-@app.route("/")
-
-def home():
-    return "Home"
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
---
+++ ApiWeb/data_manager.py
@@ -0,0 +1,27 @@
+import json
+from urllib.parse import unquote, quote_plus
+from pymongo import MongoClient
+
+class DBConnector:
+    def __init__(self, config_path='C:\\Users\\m.razzaki\\OneDrive - Biodiv-wind\\Bureau\\SAE501\\SAE501\\ApiWeb\\appsettings.json'):
+        with open(config_path, 'r') as config_file:
+            config = json.load(config_file)
+
+            # Escape the URL-encoded characters in the MONGO_URI
+            self.uri = unquote(config['MONGO_URI'])
+            self.db_name = config['DB_NAME']
+
+    def connect(self):
+        if not self.uri.startswith('mongodb://') and not self.uri.startswith('mongodb+srv://'):
+            raise ValueError('Invalid MongoDB URI: %s' % self.uri)
+
+        if '@' in self.uri:
+            # Escape the username and password in the URI
+            parts = self.uri.split('@')
+            userinfo = parts[0]
+            parts[0] = quote_plus(userinfo)
+            self.uri = '@'.join(parts)
+
+        # Specify the default database in the MongoClient constructor
+        client = MongoClient(self.uri, connect=False)
+        return client.get_database(self.db_name)
