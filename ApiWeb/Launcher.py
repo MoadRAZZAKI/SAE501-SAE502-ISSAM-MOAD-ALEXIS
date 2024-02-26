0ae516c057f2143b757753fe08ff4f5c1ec8c9fd
Moad Razzaki <m.razzaki@biodiv-wind.com>
2024-02-26 11:39:32 +0100

    commit

--- ApiWeb/Launcher.py
+++ ApiWeb/Launcher.py
@@ -5,7 +5,7 @@ from data_manager import DBConnector
 app = Flask(__name__)
 db_connector = DBConnector()
 
-# Route to get all packets
+
 @app.route('/api/data', methods=['GET'])
 def get_all_data():
     db = db_connector.connect()
@@ -13,7 +13,7 @@ def get_all_data():
     result = [{"Type": item["Type"], "Ether": item["Ether"], "IP": item["IP"], "DHCP": item["DHCP"], "UDP": item["UDP"], "BOOTP": item["BOOTP"]} for item in data]
     return jsonify(result)
 
-# Route to filter packets by type
+
 @app.route('/api/data/<packet_type>', methods=['GET'])
 def filter_data_by_type(packet_type):
     db = db_connector.connect()
@@ -21,7 +21,7 @@ def filter_data_by_type(packet_type):
     result = [{"Type": item["Type"], "Ether": item["Ether"], "IP": item["IP"], "DHCP": item["DHCP"], "UDP": item["UDP"], "BOOTP": item["BOOTP"]} for item in data]
     return jsonify(result)
 
-# Route to get DHCP packets by source MAC address
+
 @app.route('/api/data/dhcp/source_mac/<source_mac>', methods=['GET'])
 def get_dhcp_packets_by_source_mac(source_mac):
     db = db_connector.connect()
@@ -29,7 +29,7 @@ def get_dhcp_packets_by_source_mac(source_mac):
     result = [{"Type": item["Type"], "Ether": item["Ether"], "IP": item["IP"], "DHCP": item["DHCP"], "UDP": item["UDP"], "BOOTP": item["BOOTP"]} for item in data]
     return jsonify(result)
 
-# Route to get DHCP packets by destination MAC address
+
 @app.route('/api/data/dhcp/destination_mac/<destination_mac>', methods=['GET'])
 def get_dhcp_packets_by_destination_mac(destination_mac):
     db = db_connector.connect()
@@ -37,7 +37,7 @@ def get_dhcp_packets_by_destination_mac(destination_mac):
     result = [{"Type": item["Type"], "Ether": item["Ether"], "IP": item["IP"], "DHCP": item["DHCP"], "UDP": item["UDP"], "BOOTP": item["BOOTP"]} for item in data]
     return jsonify(result)
 
-# Route to get DHCP packets by source IP address
+
 @app.route('/api/data/dhcp/source_ip/<source_ip>', methods=['GET'])
 def get_dhcp_packets_by_source_ip(source_ip):
     db = db_connector.connect()
@@ -45,7 +45,7 @@ def get_dhcp_packets_by_source_ip(source_ip):
     result = [{"Type": item["Type"], "Ether": item["Ether"], "IP": item["IP"], "DHCP": item["DHCP"], "UDP": item["UDP"], "BOOTP": item["BOOTP"]} for item in data]
     return jsonify(result)
 
-# Route to get DHCP packets by destination IP address
+
 @app.route('/api/data/dhcp/destination_ip/<destination_ip>', methods=['GET'])
 def get_dhcp_packets_by_destination_ip(destination_ip):
     db = db_connector.connect()
@@ -53,7 +53,7 @@ def get_dhcp_packets_by_destination_ip(destination_ip):
     result = [{"Type": item["Type"], "Ether": item["Ether"], "IP": item["IP"], "DHCP": item["DHCP"], "UDP": item["UDP"], "BOOTP": item["BOOTP"]} for item in data]
     return jsonify(result)
 
-# Route to get DHCP packets by DHCP requested address
+
 @app.route('/api/data/dhcp/requested_address/<requested_address>', methods=['GET'])
 def get_dhcp_packets_by_requested_address(requested_address):
     db = db_connector.connect()
@@ -61,7 +61,7 @@ def get_dhcp_packets_by_requested_address(requested_address):
     result = [{"Type": item["Type"], "Ether": item["Ether"], "IP": item["IP"], "DHCP": item["DHCP"], "UDP": item["UDP"], "BOOTP": item["BOOTP"]} for item in data]
     return jsonify(result)
 
-# Route to get DHCP packets by source port
+
 @app.route('/api/data/dhcp/source_port/<source_port>', methods=['GET'])
 def get_dhcp_packets_by_source_port(source_port):
     db = db_connector.connect()
@@ -69,7 +69,7 @@ def get_dhcp_packets_by_source_port(source_port):
     result = [{"Type": item["Type"], "Ether": item["Ether"], "IP": item["IP"], "DHCP": item["DHCP"], "UDP": item["UDP"], "BOOTP": item["BOOTP"]} for item in data]
     return jsonify(result)
 
-# Route to get DHCP packets by destination port
+
 @app.route('/api/data/dhcp/destination_port/<destination_port>', methods=['GET'])
 def get_dhcp_packets_by_destination_port(destination_port):
     db = db_connector.connect()
@@ -77,7 +77,6 @@ def get_dhcp_packets_by_destination_port(destination_port):
     result = [{"Type": item["Type"], "Ether": item["Ether"], "IP": item["IP"], "DHCP": item["DHCP"], "UDP": item["UDP"], "BOOTP": item["BOOTP"]} for item in data]
     return jsonify(result)
 
-# Route to get DHCP packets by server ID
 @app.route('/api/data/dhcp/server_id/<server_id>', methods=['GET'])
 def get_dhcp_packets_by_server_id(server_id):
     db = db_connector.connect()
